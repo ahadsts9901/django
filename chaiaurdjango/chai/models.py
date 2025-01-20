e2da5cccdf3d2dfa739a7b4e30d3e1204684b4be
ahadsts9901 <ahadsts990@gmail.com>
2025-01-20 19:18:02 +0500

    bhai iska backend sir ky upper sy jarha hai

--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -1,3 +1,17 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICE = [
+        ("ML", "MASALA"),
+        ("GR", "GINGER"),
+        ("KI", "KIWI"),
+        ("EL", "ELAICHI"),
+        ("PL", "PLAIN"),
+    ]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="chais/")
+    created_at = models.DateTimeField(default=timezone.now)
+    chai_type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICE)
